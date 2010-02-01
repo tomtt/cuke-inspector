b809caf7f8d7f6bcd0cb4eedd2245fb178527149
Tom ten Thij <git@tomtenthij.nl>
2010-02-01 18:09:56 +0000

    Move template files out of public dir, add gitignore to cuke-inspector directory

--- generators/cuke_inspector_view/cuke_inspector_view_generator.rb
+++ generators/cuke_inspector_view/cuke_inspector_view_generator.rb
@@ -7,24 +7,28 @@ class CukeInspectorViewGenerator < Rails::Generator::Base
       m.directory 'public/cuke-inspector/javascripts'
       m.directory 'public/cuke-inspector/stylesheets'
 
-      m.file 'public/cuke_inspector.html', 'public/cuke-inspector/index.html'
+      m.file 'gitignore', 'public/cuke-inspector/.gitignore'
+      m.file 'javascripts/gitignore', 'public/cuke-inspector/javascripts/.gitignore'
 
-      m.file 'public/images/asc.gif',           'public/cuke-inspector/images/asc.gif'
-      m.file 'public/images/bg.gif',            'public/cuke-inspector/images/bg.gif'
-      m.file 'public/images/desc.gif',          'public/cuke-inspector/images/desc.gif'
-      m.file 'public/images/toggle-closed.gif', 'public/cuke-inspector/images/toggle-closed.gif'
-      m.file 'public/images/toggle-open.gif',   'public/cuke-inspector/images/toggle-open.gif'
+      m.file 'cuke_inspector.html', 'public/cuke-inspector/index.html'
+      m.file 'cuke_inspector.html', 'public/cuke-inspector/index.html'
 
-      m.file 'public/javascripts/cucumber_step_completion.js',
-             'public/cuke-inspector/javascripts/cucumber_step_completion.js'
-      m.file 'public/javascripts/jquery-1.3.2.min.js',
-             'public/cuke-inspector/javascripts/jquery-1.3.2.min.js'
-      m.file 'public/javascripts/jquery.tablesorter-2.0.3.min.js',
-             'public/cuke-inspector/javascripts/jquery.tablesorter-2.0.3.min.js'
+      m.file 'images/asc.gif',           'public/cuke-inspector/images/asc.gif'
+      m.file 'images/bg.gif',            'public/cuke-inspector/images/bg.gif'
+      m.file 'images/desc.gif',          'public/cuke-inspector/images/desc.gif'
+      m.file 'images/toggle-closed.gif', 'public/cuke-inspector/images/toggle-closed.gif'
+      m.file 'images/toggle-open.gif',   'public/cuke-inspector/images/toggle-open.gif'
 
-      m.file 'public/stylesheets/tablesorter.css',
+      m.file 'javascripts/cucumber_step_completion.js',
+             'cuke-inspector/javascripts/cucumber_step_completion.js'
+      m.file 'javascripts/jquery-1.3.2.min.js',
+             'cuke-inspector/javascripts/jquery-1.3.2.min.js'
+      m.file 'javascripts/jquery.tablesorter-2.0.3.min.js',
+             'cuke-inspector/javascripts/jquery.tablesorter-2.0.3.min.js'
+
+      m.file 'stylesheets/tablesorter.css',
              'public/cuke-inspector/stylesheets/tablesorter.css'
-      m.file 'public/stylesheets/cuke_inspector.css',
+      m.file 'stylesheets/cuke_inspector.css',
              'public/cuke-inspector/stylesheets/cuke_inspector.css'
     end
   end
